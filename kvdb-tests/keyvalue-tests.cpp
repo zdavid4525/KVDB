a14be44bb8a72481065f54b3c761ff8bd36a06bb
David Zhang <zdavid4525@gmail.com>
2024-03-03 01:00:59 -0500

    renamed methods to snake case

--- kvdb-tests/keyvalue-tests.cpp
+++ kvdb-tests/keyvalue-tests.cpp
@@ -1,7 +1,7 @@
 #include <catch.hpp>
 #include <kvdb.h>
 
-TEST_CASE("store and retrieve a value", "[setKeyValue,getKeyValue]") {
+TEST_CASE("store and retrieve a value", "[set_key_value,get_key_value]") {
     SECTION("basic set and get") {
         std::string db_name("emptydb");
         Database db(KVDB::create_empty_DB(db_name));
@@ -10,7 +10,7 @@ TEST_CASE("store and retrieve a value", "[setKeyValue,getKeyValue]") {
         std::string value("value");
 
         // success when retrieved value is the same as the stored value
-        db.setKeyValue(key, value);
-        REQUIRE(value == db.getKeyValue(key));
+        db.set_key_value(key, value);
+        REQUIRE(value == db.get_key_value(key));
     }
 }
