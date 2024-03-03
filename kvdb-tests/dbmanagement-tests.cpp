307de93eeee4136e98cbb90d0f8e94d5f6804ede
David Zhang <zdavid4525@gmail.com>
2024-03-03 02:00:34 -0500

    added test to load existing database instance

--- kvdb-tests/dbmanagement-tests.cpp
+++ kvdb-tests/dbmanagement-tests.cpp
@@ -7,21 +7,42 @@
 
 namespace fs = std::filesystem;
 
-TEST_CASE("create a empty, new database", "[createEmptyDB]") {
+TEST_CASE("create a empty, new database", "[create_empty_db]") {
     SECTION("Default settings") {
         std::string dbname("emptydb");
         // success when valid DB reference returned (this errors if not)
         Database db(KVDB::create_empty_DB(dbname));
 
-        // OR DB has a folder that exists in the file system
+        // AND DB has a folder that exists in the file system
         REQUIRE(fs::is_directory(fs::status(db.get_directory())));
 
-        // OR the DB folder is empty, i.e. no database files yet
+        // AND the DB folder is empty, i.e. no database files yet
         const auto &p = fs::directory_iterator(db.get_directory());
         REQUIRE(p == end(p));
 
         // teardown & check successful teardown
         db.destroy();
         REQUIRE(!fs::is_directory(fs::status(db.get_directory())));
+    }
+}
+
+TEST_CASE("load an existing database", "[load_db]") {
+    SECTION("Default settings") {
+        std::string db_name("emptydb");
+
+        //success when a valid db reference is returned (below will err if this fails)
+        Database db(KVDB::create_empty_DB(db_name));
+        Database db2(KVDB::load_db(db_name));
+
+        // AND db has a folder that exists in the file system
+        REQUIRE(fs::is_directory(fs::status(db2.get_directory())));
+
+        // AND db folder is empty
+        const auto &p = fs::directory_iterator(db2.get_directory());
+        REQUIRE(p == end(p));
+
+        // teardown & check successful teardown
+        db.destroy();
+        REQUIRE(!fs::is_directory(fs::status(db2.get_directory())));
     }
 }
